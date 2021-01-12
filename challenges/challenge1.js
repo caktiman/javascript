7c85248569fc2ef056f813887a9c65dfd26ed1f6
caktiman <52340422+caktiman@users.noreply.github.com>
2021-01-12 23:32:36 +0800

    Javascript lessons V.1.0.0

---
+++ challenges/challenge1.js
@@ -0,0 +1,16 @@
+//Porject 1
+//write a story that uses variables
+//1. Use document.write(; tp write your story onto the HTML page
+//2. MAke yoru story exiting
+//3. Join the varibales into your story using concatenation
+//4. Use prompts to retrive a value for your variables
+//5. Use 5 or more vairables in your story
+
+let age = prompt("What is your age?");
+let name =prompt("Name?")
+
+
+document.write(`Once upon a time, there's a man, named ${name}. He is age ${age}. He likes to eat rare species because he thinks that it tastes
+like chicken but with high calories. One of is favourite sign is bat because that's the sign for dinner. This is eh osidslfs `)
+
+
---
+++ lessons/lesson1.js
@@ -0,0 +1,45 @@
+//writing to Document
+document.write("Hello");
+
+//Variables-think of variables like an empty box that you can sotre things inside
+var name ="He";
+
+//es6 variables (newer version of 35!)
+document.write(`${myName} is ${age} years old`);
+
+//Prompt() allows us to get values fro users
+let favcolor = prompt("What is your fav color?");
+document.write(favcolor);
+
+//CONST is a wa to declare variables for values that are constant (they don't change)
+const student = "Tiger";
+
+//LET is a way to delcare vairbales that will change
+let teacher ="Mr. Kung";
+
+
+
+//Data Type
+//string- a string of letters (words that you write)
+//It requires you to write words that you write '' or ""
+
+//Numbers- do not user ' or " when writing numbers
+
+//Boolean- true or false value (simpy write ture or false)
+
+//Array- alist of elemts (ex. multiple numbers or strings, or both all stored in a single variable)  
+var x = []; 
+document.write(x);
+
+//Obejcts-A collection of varibales and methods that you ca use all tied to one variable
+
+//Cancatanate- allows you to join together different elements
+var myName ="htsdisfljs"
+var age = 30
+document.write(myName+"is"+age)
+
+
+
+
+
+
