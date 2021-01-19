5d0cfc76ee4bdbf1b79b1f14108ba0f370d89d8d
caktiman <52340422+caktiman@users.noreply.github.com>
2021-01-19 12:37:09 +0800

    Lesson 2 and challenge 2 complete

---
+++ lessons/lesson2.js
@@ -0,0 +1,46 @@
+//Arithemic Operaters
+//If else statement
+
+//1. Arithmetic Operators
+//A way to do math on numbers
+
+// = Addition
+// - Substraction
+// * Multiply
+// ** exponentatiation (Es2016)
+// / Divide
+// % Modulus (Remainder) Ex: 5/2=2 Remainder=1 Therfore it should show 1
+// ++ increment
+// -- Decrement
+
+//Comparitive Oeprators
+// < Less than
+// > Greater
+// == is equal to (comapres teh value)
+// === is equal to (compares teh value AND type)
+// !== is NOT equal to 
+
+var x= "7";
+var y = 7;
+
+document.write(x === y);
+
+
+
+//document.write ( ++ );
+
+//2. If ELse statements
+//computer logic to execute code, if a condition is true.
+//the else is the default in cse the fi is not true
+
+if(5 < 2){
+  //  document.write("Yes"); If condition is true
+}
+else if{
+    Condition is true, it will execute.
+}
+
+else{
+document.write("No");  If condition is false
+}
+
