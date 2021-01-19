484734ec670be73ab6bc7dca8ddfd8318f066389
caktiman <jw5505869@gmail.com>
2021-01-19 18:17:52 +0800

    Challenge 1 Finished

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -1,16 +1,24 @@
 //Porject 1
 //write a story that uses variables
-//1. Use document.write(; tp write your story onto the HTML page
-//2. MAke yoru story exiting
+//1. Use document.write(); tp write your story onto the HTML page
+//2. MAke your story exiciting
 //3. Join the varibales into your story using concatenation
 //4. Use prompts to retrive a value for your variables
 //5. Use 5 or more vairables in your story
 
-let age = prompt("What is your age?");
-let name =prompt("Name?")
+let age = prompt("Random age?");
+let name = prompt("Name?")
+let wish = prompt("Name your wish?") 
+let animal = prompt("name a animal?")
+var IDONTKNOW = "After that, I don't know what happened to him/her/it anymore. So that will be the end of the story. I guess?"
 
 
-document.write(`Once upon a time, there's a man, named ${name}. He is age ${age}. He likes to eat rare species because he thinks that it tastes
-like chicken but with high calories. One of is favourite sign is bat because that's the sign for dinner. This is eh osidslfs `)
+document.write(`Once upon a time, there's a human, named ${name}. He/She/It is age ${age}. He/She/It likes to eat rare species because he/she thinks that it tastes
+like chicken but with high calories. One of his/her/its favourite sign is bat because that's the sign for dinner. He/She/It suddenly came up with an idea. ${wish}. 
+That's when he/she/it found a/an ${animal}. He/She/It decided to adopt this little poor thing just because he/she/it wanted to. During his/her/its journey, he/she/it found many liitle pinkish human beings that shouted "Free Taiwan!!!" "All Hail to our party!"
+Because he/she/it felt familiar.He/She/It decided to join them. He/She/It later became the leader of these pinkish being. He/She/It declared war against every contry in the 
+world and ended missing.${IDONTKNOW}`)
+
+
 
 
