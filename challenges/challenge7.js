c996a077b7b954295baccb3ff105680223fce0dc
caktiman <52340422+caktiman@users.noreply.github.com>
2021-03-24 15:52:59 +0800

    Big update: Graphics + Menu Developed

--- challenges/challenge7.js
+++ challenges/challenge7.js
@@ -1,13 +1,39 @@
+// function pic(){
+//   document.querySelector("#bgimg").style.backgroundImage = 'url("../images/void.png")';
+// }
 
-document.querySelector("#image").style.backgroundImage = "images\Beelzebub.png"
 
 function start(){
-    var enter = doucment.getElementNById('start')
+  console.log('start function ran')
+  var enter = document.getElementById('start')
+  document.querySelector('#bgimg').style.backgroundImage = "url('../images/Beelzebub.png')"
+  document.getElementById("title").innerHTML="Belzebub, The Great Fly"
+  document.getElementById("description").innerHTML="Hi you, plz don’t mind me la. It’s just me, a fly called Beelzebub la."
+  // document.querySelector('.option1').style.display = "block"
+  // document.querySelector('.option2').style.display = "block"
+
+  document.querySelector('.option1').style.display = "block"
+  document.querySelector('.option2').style.display = "block"
+
+  setTimeout(() => {
+    document.querySelector('.option1').style.opacity = "1"
+    document.querySelector('.option1').style.transform = "none"
+    document.querySelector('.option2').style.opacity = "1"
+    document.querySelector('.option2').style.transform = "none"
+  }, 100)
 }
+
 
 function quit(){
     window.close()
 }
+
+document.addEventListener("keyup", function(press) {
+  if (press.keyCode === 13) {
+   press.preventDefault();
+   start()
+  }
+})
 
 enter.addEventListener('enter',function(){
   alert("it works");
