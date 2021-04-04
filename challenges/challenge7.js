e34b14c85e3bbd066c36e9f48c26525af8345484
caktiman <52340422+caktiman@users.noreply.github.com>
2021-04-04 14:41:09 +0800

    abyss img updated+ other images updates

--- challenges/challenge7.js
+++ challenges/challenge7.js
@@ -121,11 +121,72 @@ function determination(){
     document.getElementById("title").innerHTML=""
     document.getElementById("description").innerHTML="You have nothing to lose anyway, so you decided to drive down to hell with a beloved modified “Bloody Mary”"  
   
-    document.removeEventListener("keyup", determination) 
-    document.addEventListener("keyup", beginsJourney)
+    document.querySelector('.option1').style.display = "block"
+    document.querySelector('.option1').innerHTML = "OK..."
+    document.querySelector('.option1').style.opacity = "1"
+    document.querySelector('.option1').style.transform = "none"
+    document.querySelector('.option1').onclick = firstTransition
+
+    document.removeEventListener("keyup", determination)
+    document.removeEventListener("keyup", pandemonia)
+    
   }
 }
 
+function firstTransition(){
+  introMusic.load()
+  mainMusic.play()
+  
+  document.querySelector('.option1').style.display = ""
+  document.querySelector('#bgimg').style.backgroundImage = "url('../images/coffee shop.png')"
+  document.getElementById("title").innerHTML=""
+  document.getElementById("description").innerHTML="As you traveled  into the hell, you met a girl dressed up as an OL, sipping her coffee."
+
+  document.removeEventListener("keyup", firstTransition) 
+  document.addEventListener("keyup", pandemonia)
+}
+
+function pandemonia(event){
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector('#bgimg').style.backgroundImage = "url('../images/Pandemonia.png')"
+    document.getElementById("title").innerHTML="Pandemonia, the Tired Demon"
+    document.getElementById("description").innerHTML="Name’s Pandemonica, Hell’s Customer Service. How may I serve you?"  
+  
+    document.removeEventListener("keyup", pandemonia) 
+    document.addEventListener("keyup", pandemoniaChoice)
+  }
+}
+
+function pandemoniaChoice(event) {
+  if (event.keyCode === 13 ) {
+    document.getElementById("description").innerHTML=""
+    document.querySelector('.option1').style.display = "block"
+    document.querySelector('.option2').style.display = "block"
+    document.querySelector('.option1').innerHTML = "We can figure something out at my place"
+    document.querySelector('.option2').innerHTML = "Maybe I can serve YOU instead?"
+    document.querySelector('.option1').onclick = panSuccess
+    document.querySelector('.option1').onclick = panFail
+
+    setTimeout(() => {
+      
+      document.querySelector('.option1').style.opacity = "1"
+      document.querySelector('.option1').style.transform = "none"
+      
+      document.querySelector('.option2').style.opacity = "1"
+      document.querySelector('.option2').style.transform = "none"
+      
+    }, 200)
+  }
+}
+
+function panSuccess(event) {
+  
+}
+
+function pandFail(event){
+
+}
 
 
 
