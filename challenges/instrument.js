decab7e931b01627fa095903f485cddf593e6901
caktiman <52340422+caktiman@users.noreply.github.com>
2021-04-27 12:43:15 +0800

    Piano update

---
+++ challenges/instrument.js
@@ -0,0 +1,252 @@
+var c3 = new Audio("instrument music/")
+var c33 = new Audio("instrument music/")
+var d3 = new Audio("instrument music/")
+var d33 = new Audio("instrument music/")
+var e3 = new Audio("instrument music/")
+var f3 = new Audio("instrument music/")
+var f33 = new Audio("instrument music/")
+var g3 = new Audio("instrument music/")
+var g33 = new Audio("instrument music/")
+var a3 = new Audio("instrument music/")
+var a33 = new Audio("instrument music/")
+var b3 = new Audio("instrument music/")
+var b33 = new Audio("instrument music/")
+var c4 = new Audio("instrument music/")
+var c44 = new Audio("instrument music/")
+var d4 = new Audio("instrument music/")
+var d44 = new Audio("instrument music/")
+var e4 = new Audio("instrument music/")
+var f4 = new Audio("instrument music/")
+var f44 = new Audio("instrument music/")
+var g4 = new Audio("instrument music/")
+var g44 = new Audio("instrument music/")
+var a4 = new Audio("instrument music/")
+var a44 = new Audio("instrument music/")
+var b4 = new Audio("instrument music/")
+var c5 = new Audio("instrument music/")
+var c55 = new Audio("instrument music/")
+var d5 = new Audio("instrument music/")
+var d55 = new Audio("instrument music/")
+var e5 = new Audio("instrument music/")
+
+
+
+document.querySelector('#c3').onclick = playC3
+function playC3() {
+    c3.play()
+    setTimeout(() => {
+        c3.load()
+    }, 500)
+}
+
+document.querySelector('#c33').onclick = playC33
+function playC33() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#d3').onclick = playD3
+function playD3() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#d33').onclick = playD33
+function playD33() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#e3').onclick = playE3
+function playE3() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#f3').onclick = playF3
+function playF3() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#f33').onclick = playF3
+function playF33() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#g3').onclick = playG3
+function playG3() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#g33').onclick = playG33
+function playG33() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#a3').onclick = playA3
+function playA3() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#a33').onclick = playA33
+function playA33() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#b3').onclick = playB3
+function playB3() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#b33').onclick = playB33
+function playB33() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#c4').onclick = playC4
+function playC4() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#c44').onclick = playC44
+function playC44() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#d4').onclick = playD4
+function playD4() {
+    setTimeout(() => {
+        
+    }, 500)
+}
+
+document.querySelector('#d44').onclick = playD44
+function playD44() {
+    c44.play()
+    setTimeout(() => {
+        c44.load()
+    }, 500)
+}
+
+document.querySelector('#e4').onclick = playE4
+function playE4() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#f4').onclick = playF4
+function playF4() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#f44').onclick = playF44
+function playF44() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#g4').onclick = playG3
+function playG4() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#g44').onclick = playG44
+function playG44() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#a4').onclick = playA4
+function playA4() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#a44').onclick = playA44
+function playA44() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#b4').onclick = playB4
+function playB4() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#b44').onclick = playB44
+function playB44() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#c5').onclick = playC5
+function playC5() {
+    c3.play()
+    setTimeout(() => {
+        c3.load()
+    }, 500)
+}
+
+document.querySelector('#c55').onclick = playC55
+function playC55() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#d5').onclick = playD5
+function playD5() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#d55').onclick = playD55
+function playD55() {
+    setTimeout(() => {
+       
+    }, 500)
+}
+
+document.querySelector('#e5').onclick = playE5
+function playE5() {
+    setTimeout(() => {
+       
+    }, 500)
+}
