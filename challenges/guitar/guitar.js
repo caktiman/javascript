03882f2f74216522a16627378b87315556815c28
caktiman <52340422+caktiman@users.noreply.github.com>
2021-05-07 12:29:29 +0800

    Guitar complete

--- challenges/guitar/guitar.js
+++ challenges/guitar/guitar.js
@@ -1,8 +1,43 @@
+var lowE = new Audio("Guitar low e.m4a")
+var  A= new Audio("Guitar A rev.m4a")
+var D = new Audio("Guitar d.m4a")
+var G = new Audio("Guitar g.m4a")
+var B = new Audio("Guitar b.m4a")
+var highE = new Audio("Guitar high e rev.m4a")
+
 document.addEventListener("mouseover", check)
 
 function check(event){
-    console.log(event)
-    if (event.mouseover === string1){
-        e.play()
+    console.log(event.explicitOriginalTarget.id)
+    if (event.explicitOriginalTarget.id === "1"){
+        lowE.play()
+        setTimeout(() => {
+            lowE.load()
+        }, 4000)
+    }else if(event.explicitOriginalTarget.id === "2"){
+        A.play()
+        setTimeout(() => {
+            A.load()
+        }, 4000)
+    }else if(event.explicitOriginalTarget.id === "3"){
+        D.play()
+        setTimeout(() => {
+            D.load()
+        }, 4000)
+    }else if(event.explicitOriginalTarget.id === "4"){
+        G.play()
+        setTimeout(() => {
+            G.load()
+        }, 4000)
+    }else if(event.explicitOriginalTarget.id === "5"){
+        B.play()
+        setTimeout(() => {
+            B.load()
+        }, 4000)
+    }else if(event.explicitOriginalTarget.id === "6"){
+        highE.play()
+        setTimeout(() => {
+            highE.load()
+        }, 4000)
     }
 }
